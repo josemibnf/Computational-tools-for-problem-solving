e8724555de62a5589729e793dc1a7bd347a6ee58
josemibnf <josemi.bnf@outlook.es>
2020-10-21 17:30:43 +0200

    ..

--- algorithms.py
+++ algorithms.py
@@ -4,9 +4,31 @@ def extended_euclidean_algorithm(x, y, s=1, t=0, S=0, T=1) -> float: return exte
 
 def fermat_primality__test(n, a=1) -> bool: return (fermat_primality__test(n=n, a=a+1) if a^(n-1)%n==1 or a^(n-1)%n==-1 else False ) if a<n else True
 
-def is_prime(n): return False
+def fermat_factory_method(n) -> tuple:
+    import math
+    k = math.ceil(math.sqrt(n))
+    h = k**2-n
+    sh = math.sqrt(abs(h))
+    while not sh.is_integer():
+        k +=1
+        h = k**2-n
+        sh = math.sqrt(abs(h))
+    return k-sh, k+sh
 
-def find_carmichael_numbers(n) -> list: return [ a for a in range(1, n) if fermat_primality__test(n=a) and not is_prime(a) ]
+def euler_function(n) -> int: return len([ m for m in range(1, n) if m<n and euclidean_algorithm(n,m)==1 ])
+
+def rsa_public_key_generator(n, p, q) -> int:
+    fi = (p-1)*(q-1)
+    for e in reversed(range(1, fi)):
+        if euclidean_algorithm(fi, e)==1:
+            return e
+
+def rsa_private_key_generator(n, e) -> int:
+    for d in range(0, n):
+        if (e*d)%n==1:
+            return d
+
+def find_carmichael_numbers(n) -> list: return [ a for a in range(1, n) if fermat_primality__test(n=a) and not miller_rabin(n=a) ]
 
 def miller_rabin_primality_test(n) -> bool: return miller_rabin_primality_test(n=n) if fermat_primality__test(n=n) and fermat_primality__test(n=n) else False
 
@@ -43,22 +65,29 @@ def miller_rabin(n, k=40):
             return False
     return True
 
-def rabin_key_generator() -> int:
-    import random
-    p=11
-    q=17
-    return p*q, p, q
+class Rabin:
+    def rabin_key_generator(self) -> int:
+        import random
+        p=11
+        q=17
+        return p*q, p, q
 
-def rabin_encryption(message) -> int: 
-    n = rabin_key_generator()[0]
-    return int(message)^2%n if int(message) < n else None
+    def rabin_encryption(self, message) -> int: 
+        n = self.rabin_key_generator()[0]
+        return int(message)^2%n if int(message) < n else None
 
-def rabin_decryption(crypted, p, q) -> tuple:
-    yp, yq = extended_euclidean_algorithm(p, q)
-    mq, mp = crypted^((p+1)/4)%p, crypted^((q+1)/4)%q
-    return (
-        yp*p*mq+yq+q+mp,
-        p*q-yp*p*mq+yq+q+mp,
-        yp*p*mq-yq*q*mp,
-        p*q-yp*p*mq-yq*q*mp
-    )
+    def rabin_decryption(self, crypted, p, q) -> tuple:
+        yp, yq = extended_euclidean_algorithm(p, q)
+        mq, mp = crypted^((p+1)/4)%p, crypted^((q+1)/4)%q
+        return (
+            yp*p*mq+yq+q+mp,
+            p*q-yp*p*mq+yq+q+mp,
+            yp*p*mq-yq*q*mp,
+            p*q-yp*p*mq-yq*q*mp
+        )
+
+if __name__ == "__main__":
+    n = 17947
+    e= 3929
+    fi = euler_function(n)
+    print(  )
